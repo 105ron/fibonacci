3e0844f6add23f727fc2771a8c2303b150865dd5
rhys <rbrooker105@gmail.com>
2016-09-07 16:12:12 +1000

    Recursion fibs working

--- fibonacci.rb
+++ fibonacci.rb
@@ -5,3 +5,36 @@ def fibs(n)
   end
   fibonacci
 end
+
+def fibs_rec(n, array = [])
+  if n == 0 
+  	return 0
+  	puts array
+  end
+  if n == 1 
+  	return 1
+  else
+  	array[n] = fibs_rec(n - 1) + fibs_rec(n -2)
+
+  end
+end
+  
+  
+def fibs_rec(n, iteration = 0, array = [])
+  if iteration == 0
+  	array = [0]
+  elsif iteration == 1
+  	array = [0,1]
+  else
+  	array[iteration] = array[iteration -1] + array[iteration -2]
+  end
+  iteration += 1
+  if (n + 1) == iteration
+  	return array
+  else
+  	fibs_rec(n, iteration, array)
+  end
+
+end
+
+
